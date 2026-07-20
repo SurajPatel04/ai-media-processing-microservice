680799803e21bf4d0316d83b75997698edbefc5c
Suraj Patel <surajpatel9390@gmail.com>
2026-07-20 18:20:11 +0000

    auth route update and new dependency added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -8,11 +8,12 @@ import {
 import { validate } from "../middlewares/validate.middleware.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { registerSchema, loginSchema } from "../schema/auth.schema.js";
+import { authRateLimiter } from "../middlewares/rateLimiter.middleware.js";
 
 const router = Router();
 
-router.post("/register", validate(registerSchema), registerUser);
-router.post("/login", validate(loginSchema), loginUser);
+router.post("/register", authRateLimiter, validate(registerSchema), registerUser);
+router.post("/login", authRateLimiter, validate(loginSchema), loginUser);
 router.post("/refresh-token", refreshAccessToken);
 router.post("/logout", authenticate, logoutUserFromAllDevices);
 
